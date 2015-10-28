11878b08f0fdd4bdcd7b02a99448c8ab0c73382d
Rukeith <fenglan.crazydance@gmail.com>
2015-10-29 01:09:17 +0800

    update chapter 2

---
+++ example/tutorial2.js
@@ -0,0 +1,20 @@
+// tutorial2.js
+var CommentList = React.createClass({
+  render: function() {
+    return (
+      <div className="commentList">
+        Hello, world! I am a CommentList.
+      </div>
+    );
+  }
+});
+
+var CommentForm = React.createClass({
+  render: function() {
+    return (
+      <div className="commentForm">
+        Hello, world! I am a CommentForm.
+      </div>
+    );
+  }
+});
---
+++ example/tutorial3.js
@@ -0,0 +1,12 @@
+// tutorial3.js 
+var  CommentBox  =  React . createClass ({ 
+  render :  function ()  { 
+    return  ( 
+      < div  className = "commentBox" > 
+        < h1 > Comments < /h1> 
+        < CommentList  /> 
+        < CommentForm  />
+       < /div> 
+    ) ; 
+  } 
+});
